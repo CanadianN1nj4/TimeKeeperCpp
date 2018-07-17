93b04ad3d47daf6bb475eb09608272d36f920ba1
tomo <tsutlovic@gmail.com>
2018-07-17 15:58:08 -0400

    Added imports, basic exit option and the list of future options

--- TimeKeeper.cpp
+++ TimeKeeper.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  std::cout << "Choose an option\n" << "1. Start Timer\n" << "2. Check Timer\n" << "3. Edit Categories\n" << "4. Edit Events\n" << "5. Export\n" << "6. Reset Timer\n" << "7. Exit" << endl ;
+
+
+
+  std::cout << "Press any key to exit" << '\n';
+  cin.ignore();
+  return 0;
+}
